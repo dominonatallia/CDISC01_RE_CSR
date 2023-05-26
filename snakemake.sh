0c813db19c3c116d3666efb41301e3831fa4dd97
Tom Ratford <tom.ratford@veramed.co.uk>
2023-05-26 11:12:37 +0100

    FIX: unhardcode paths

--- snakemake.sh
+++ snakemake.sh
@@ -1,1 +1,1 @@
-snakemake --cores 1 --directory "/mnt/data/$DOMINO_PROJECT_NAME/" && snakemake --report '/mnt/data/CDISC01_RE_INTERIM/report.html' --directory '/mnt/data/CDISC01_RE_INTERIM/'
+snakemake --cores 1 --directory "/mnt/data/$DOMINO_PROJECT_NAME/" && snakemake --report "/mnt/data/$DOMINO_PROJECT_NAME/report.html" --directory "/mnt/data/$DOMINO_PROJECT_NAME/"
