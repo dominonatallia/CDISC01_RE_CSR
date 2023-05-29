fe1b218859a659f1deb1437332c4b091271de79a
Stuart Malcolm <73795478+metadatadriven@users.noreply.github.com>
2023-05-29 14:18:42 +0100

    Update multijob.sh
    use python cmd

--- multijob.sh
+++ multijob.sh
@@ -1,2 +1,1 @@
-chmod +x /mnt/imported/code/SCE_STANDARD_LIB/Pipelines/multijob.py
-'/mnt/imported/code/SCE_STANDARD_LIB/Pipelines/multijob.py' jobs.cfg
+python /mnt/imported/code/SCE_STANDARD_LIB/Pipelines/multijob.py jobs.cfg
